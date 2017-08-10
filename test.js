b408f8ea294a83ffb46804e7f257359a5d38f0f0
Samuel Kaiser <samuel.kaiser01@gmail.com>
2017-08-10 09:24:59 +0200

    Improve test, will adjust htl_active_directory
    Sorry for committing like this, I'm literally coding on GitHub.com, will test everything later

--- test.js
+++ test.js
@@ -5,19 +5,21 @@ if (typeof process.argv[2] !== "string" || typeof process.argv[3] !== "string") 
     throw "Ungültiger Aufruf. Syntax: node test.js username passwort.";
 }
 
-var HtlActiveDirectory = require("./htl_active_directory");
-var htlAd = new HtlActiveDirectory();
+var htlActiveDirectory = require("./htl_active_directory");
 
 var sentUsername = process.argv[2];
 var sentPassword = process.argv[3];
 
-htlAd.login(sentUsername, sentPassword,
+htlActiveDirectory(sentUsername, sentPassword,
     /* onSuccess bei Login */
-    function () {
+    function (err, session) {
+        if (err) {
+            console.error(err);
+        }
         console.log("Login OK.");
         /* Hier können auch Gruppenmitgliedschaften anderer User gesucht werden. Das AD schränkt dieses
          * nicht ein. */
-        htlAd.getGroupMembership(sentUsername,
+        session.getGroupMembership(sentUsername,
             /* onSuccess */
             function (groups) {
                 console.log("Gruppen des Users: ", groups);
@@ -28,7 +30,7 @@ htlAd.login(sentUsername, sentPassword,
             });
 
         console.log("Welche Member hat die Gruppe AlleLehrende?");
-        htlAd.getUsersOfGroup("AlleLehrende",
+        session.getUsersOfGroup("AlleLehrende",
             /* onSuccess von getUsersOfGroup */
             function (userlist) {
                 console.log("Members von AlleLehrende", userlist);
@@ -42,14 +44,10 @@ htlAd.login(sentUsername, sentPassword,
          * Doku siehe https://www.npmjs.com/package/activedirectory */
         console.log("Detaillierte Infos zum übermittelten User");
         /* Das geht auch bei jedem anderen User */
-        htlAd.adInstance.findUser(sentUsername, function (err, user) {
+        session.adInstance.findUser(sentUsername, function (err, user) {
             if (user) {
                 console.log(user);
             }
         });
-    },
-    /* onError bei Login */
-    function (message, innerMessage) {
-        console.log(message, innerMessage);
     }
 );
